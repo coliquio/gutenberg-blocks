6b0eda85868a03f18f530effbd76b0b588e8c774
Nazarii Goncharuk <nazarii.goncharuk@L0232.local>
2020-06-24 16:46:01 +0200

    branding block ready

--- src/branding/index.js
+++ src/branding/index.js
@@ -2,26 +2,26 @@ import React from 'react'
 import {blockEditor, components, i18n} from 'wp'
 import './style.scss'
 
-
 const { __ } = i18n
-const { TextControl, PanelBody, SelectControl } = components
+const { TextControl } = components
 const { InnerBlocks } = blockEditor
 
 export const name = 'branding-box'
 
-/**
- * Visual variants for this highlight box
- */
 const TEMPLATE = [
-  ['coliquio/image', { placeholder: 'Recipe Title123', onChange123: (title) => setAttributes({ title }) }],
+  ['coliquio/image', {
+      displayCopyright: false,
+      displayCaption: false,
+    },
+  ],
 ]
 
 export const settings = {
-  title: __('BrandingBox'),
+  title: __('Branding Box'),
 
   description: __('Visual wrapper with different styles'),
 
-  icon: 'cover-image',
+  icon: 'tickets-alt',
 
   attributes: {
     title: {
@@ -38,7 +38,7 @@ export const settings = {
     return (
       <div className={ className }>
         <TextControl
-          label={__('Box Title')}
+          label={__('Branding Title')}
           value={title || ''}
           onChange={title => setAttributes({ title })}
         />
@@ -49,9 +49,20 @@ export const settings = {
     )
   },
 
-  save({ attributes }) {
+  save({ attributes, className }) {
+    const {
+      title,
+    } = attributes;
+
     return (
-      <InnerBlocks.Content />
+      <div className={ className }>
+        <div class="branding-box-outer-wrapper">
+          <aside class="branding-box-inner-wrapper">
+            {title && <span className="branding-title">{title}</span>}
+            <InnerBlocks.Content />
+          </aside>
+        </div>
+      </div>
     )
   },
 }
