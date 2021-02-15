a3f31ab8ec53c9d021462280c512f09b604d51b0
Nazarii Goncharuk <nazarii.goncharuk@L0232.local>
2021-02-15 09:23:55 +0100

    test crop options update

--- src/image/extend.js
+++ src/image/extend.js
@@ -308,7 +308,7 @@ const withSrcAttribute = createHigherOrderComponent( ( BlockEdit ) => {
           copyright: get(image, 'media_fields.field_copyright.value.value'),
           size: !props.attributes.size ? sizeControlOptions.find(o => o.default).value : props.attributes.size,
           className: !props.attributes.size ? `custom-size-${ sizeControlOptions.find(o => o.default).value }` : props.attributes.className,
-          crop: (crop && props.selectedCrop ? {
+          crop: (crop && props.attributes.selectedCrop ? {
             name: crop.name,
             width: crop.width,
             height: crop.height,
