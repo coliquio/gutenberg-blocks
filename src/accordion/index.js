bcac0e467e875cfdf47e024fb4fb75741b342330
Nebahat Gueney <nebahat.gueney@coliquio.de>
2021-02-25 13:40:07 +0100

    test pre-commit linting

--- src/accordion/index.js
+++ src/accordion/index.js
@@ -1,22 +1,21 @@
 /**
  * External dependencies
  */
-import React from 'react'
-import { editor, element, i18n } from 'wp'
+import React from 'react';
+import { editor, element, i18n } from 'wp';
 /**
  * Internal dependencies
  */
-import './style.scss'
+import './style.scss';
 
+const { Fragment } = element;
+const { __ } = i18n;
 
-const { Fragment } = element
-const { __ } = i18n
+const { RichText, InnerBlocks } = editor;
 
-const { RichText, InnerBlocks } = editor
+export const name = 'accordion';
 
-export const name = 'accordion'
-
-const ALLOWED_BLOCKS = [ 'coliquio/accordion-item' ];
+const ALLOWED_BLOCKS = ['coliquio/accordion-item'];
 
 export const settings = {
   title: __('Accordion'),
@@ -31,41 +30,41 @@ export const settings = {
     },
   },
 
-  edit({ attributes, className, setAttributes }) {
+  edit ({ attributes, setAttributes }) {
     return (
       <Fragment>
-        <div class="accordion-component">
+        <div className="accordion-component">
           <RichText
             tagName="p"
-            placeholder={ __( 'Accordion Title(optional)' ) }
-            value={ attributes.accordionTitle }
+            placeholder={__('Accordion Title(optional)')}
+            value={attributes.accordionTitle}
             className="accordion-component-header"
-            onChange={ ( value ) =>
-              setAttributes( { accordionTitle: value } )
-            }
+            onChange={value => setAttributes({ accordionTitle: value })}
           />
           <div>
-            <InnerBlocks
-              allowedBlocks={ ALLOWED_BLOCKS }
-            />
+            <InnerBlocks allowedBlocks={ALLOWED_BLOCKS} />
           </div>
         </div>
       </Fragment>
-    )
+    );
   },
 
-  save({ attributes, className }) {
+  save ({ attributes }) {
     let titleBlock = '';
     if (attributes.accordionTitle) {
-      titleBlock = <summary class="accordion-header"><RichText.Content value={ attributes.accordionTitle } /></summary>;
+      titleBlock = (
+        <summary className="accordion-header">
+          <RichText.Content value={attributes.accordionTitle} />
+        </summary>
+      );
     }
     return (
       <div>
-        { titleBlock }
+        {titleBlock}
         <div>
           <InnerBlocks.Content />
         </div>
       </div>
-    )
+    );
   },
-}
+};
