83749d7f096233c344a3466eee46cebe234ca57c
Nebahat Gueney <nebahat.gueney@coliquio.de>
2021-03-18 17:38:10 +0100

    remove token

--- graphql/client.js
+++ graphql/client.js
@@ -6,6 +6,6 @@ const endpoint = 'https://api.coliquio.integration.coliqu.io/graphql';
 
 export const client = new GraphQLClient(endpoint, {
   headers: {
-    Authorization: 'Bearer 0a3eec6676d6817a8a20072b9b0a2b9e',
+    Authorization: '',
   },
 });
