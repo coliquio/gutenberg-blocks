32fac72a1a7e911bf7ef65d4a81522729e1af8e6
Nader Safadi <nader.safadi@coliquio.de>
2023-05-05 12:34:46 +0200

    fix(AP-36): Remove custom styles for the file block.

--- src/file/extend.js
+++ src/file/extend.js
@@ -1,19 +1,11 @@
-import React from 'react';
 import assign from 'lodash.assign';
-import { blockEditor, components, compose, element, hooks, i18n } from 'wp';
+import { hooks } from 'wp';
 import './style.scss';
 
-const { createHigherOrderComponent } = compose;
-const { Fragment } = element;
 const { addFilter } = hooks;
-const { __ } = i18n;
-const { InspectorControls } = blockEditor;
-const { PanelBody, RadioControl, ToggleControl } = components;
 
 // Enable properties on the following blocks
-const enableOnBlocks = [
-  'core/file',
-];
+const enableOnBlocks = ['core/file'];
 
 /**
  * Add extra attributes to block.
@@ -31,44 +23,18 @@ const addExtraAttributes = (settings, name) => {
 
   settings.attributes = assign(settings.attributes, {
     textLinkTarget: {
-      type: "string",
-      default: "",
-    }
+      type: 'string',
+      default: '',
+    },
   });
 
   return settings;
 };
 
 addFilter(
-  "blocks.registerBlockType",
-  "extend-block-file/attribute/extra-attributes",
-  addExtraAttributes
+  'blocks.registerBlockType',
+  'extend-block-file/attribute/extra-attributes',
+  addExtraAttributes,
+  'blocks.getSaveContent.extraProps',
+  'extend-block-image/get-save-content/extra-props-styles',
 );
-
-/**
- * Add margin style attribute to save element of block.
- *
- * @param {object} saveElementProps Props of save element.
- * @param {Object} blockType Block type information.
- * @param {Object} attributes Attributes of block.
- *
- * @returns {object} Modified props of save element.
- */
-const addExtraPropsStyle = ( saveElementProps, blockType, attributes ) => {
-  if ( ! enableOnBlocks.includes( blockType.name ) ) {
-      return saveElementProps;
-  }
-  wp.blocks.registerBlockStyle( 'core/file', {
-    name: 'link',
-    label: 'Link',
-  });
-  wp.blocks.registerBlockStyle( 'core/file', {
-    name: 'button',
-    label: 'Button'
-  });
-
-  return saveElementProps;
-};
-
-addFilter( 'blocks.getSaveContent.extraProps', 'extend-block-image/get-save-content/extra-props-styles', addExtraPropsStyle );
-
